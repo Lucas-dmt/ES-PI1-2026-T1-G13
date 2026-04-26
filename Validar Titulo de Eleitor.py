b996821cfcde72cd97043462dc38e42746fc2e2f
gustavomm18 <gugu.missawa@gmail.com>
2026-04-25 23:13:11 -0300

    Update Validar Titulo de Eleitor.py

--- Validar Titulo de Eleitor.py
+++ Validar Titulo de Eleitor.py
@@ -6,7 +6,7 @@ else:
 
     for c in titulo_eleitor:
         if c < "0" or c > "9":
-            valido = False
+            titulovalido = False
 
     if not titulo_valido:
         print("Título inválido: só pode conter números")
@@ -37,43 +37,4 @@ else:
         else:
             print("Título inválido: dígitos não conferem")
 
-
-
-if cpf_valido and titulo_valido:
-  
-    import time
-    import random
-
-def gerar_chave():
-    print("\nCadastro de título e CPF registrado")
-    print("Estamos gerando sua chave...")
-
-    time.sleep(5)
-
-    chave = f"{random.randint(0, 999999):06}"
-
-    print(f"Sua chave é: {chave}")
-
-    return chave
-
-
-def escolher():
-    x = int(input("\nDigite 2 para ir no menu de votação ou 11 para voltar ao gerenciamento: "))
-
-    if x == 2:
-        print("Indo para o menu de votação... ")
-
-    elif x == 11:
-        print("Voltando ao gerenciamento...")
-
-    else:
-        print("Resposta inválida ")
-        x = int(input("\nDigite 2 para ir no menu de votação ou 11 para voltar ao gerenciamento: "))
-    
-
-gerar_chave()
-escolher()
-
-else:
-    print("\nCadastro inválido")
 
