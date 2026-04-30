c2335298dc8196eabb7e5ba48218ccfb0df29279
gustavomm18 <gugu.missawa@gmail.com>
2026-04-29 21:42:41 -0300

    Update correção da chave.py

--- correção da chave.py
+++ correção da chave.py
@@ -1,5 +1,11 @@
+import time
 import random
+def gerar_chave(menu_votacao, menu_gerenciamento):
+    print("\nCadastro de título e CPF registrado")
+    print("Estamos gerando sua chave...")
 
+    time.sleep(5)
+    
 nome = input("Digite seu nome completo: ")
 
 letras = nome[:2]# as 2 primeiras letras do nome
@@ -11,3 +17,20 @@ for i in range(len(nome)):
 chave = letras.upper() + f"{random.randint(1000, 9999)}"
 
 print("Sua chave é:", chave)
+
+x = 0
+
+    while x != 2 and x != 11:
+        x = int(input("\nDigite 2 para votação ou 11 para gerenciamento: "))
+
+        if x == 2:
+            print("Indo para votação...")
+            menu_votacao()
+
+        elif x == 11:
+            print("Voltando ao gerenciamento...")
+            menu_gerenciamento()
+
+        else:
+            print("Opção inválida\n")
+    return chave
