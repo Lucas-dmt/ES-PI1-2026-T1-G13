9a6fca70f634563394f1dde7b2b8250847091eb7
gustavomm18 <gugu.missawa@gmail.com>
2026-04-29 21:37:36 -0300

    Update Reuniao 3.py

--- Reuniao 3.py
+++ Reuniao 3.py
@@ -1,4 +1,3 @@
 Requisitos para correção da reuniao 2:
-1- arrumar codigo de geração de chave,contendo 3 letras;2 primeiras letras do primeiro nome e 1 primeira letra do segundo nome e 4 numeros aleatorios
-2- corrigir a verificacao de voto se sim ou não
-3- 
+1- corrigir a verificacao de voto se sim ou não
+2- 
