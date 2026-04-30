3a659fa4c91d13014f10d936be923d4235ce50c8
gustavomm18 <gugu.missawa@gmail.com>
2026-04-29 21:48:16 -0300

    Update correção da chave.py

--- correção da chave.py
+++ correção da chave.py
@@ -1,24 +1,27 @@
 import time
 import random
+
 def gerar_chave(menu_votacao, menu_gerenciamento):
+    
+    nome = input("Digite seu nome completo: ")
+
     print("\nCadastro de título e CPF registrado")
     print("Estamos gerando sua chave...")
 
     time.sleep(5)
-    
-nome = input("Digite seu nome completo: ")
 
-letras = nome[:2]# as 2 primeiras letras do nome
+    letras = nome[0] + nome[1]# as 2 primeiras letras do nome
 
-for i in range(len(nome)):
-    if nome[i] == " ":
-        letras += nome[i + 1]# soma as 2 primeiras letras,pula o espaço e junta a primeira letra do sobrenome depois do i espaço
+    for i in range(len(nome)):
+        if nome[i] == " ":
+            letras += nome[i + 1]# soma as 2 primeiras letras,pula o espaço e junta a primeira letra do sobrenome depois do i espaço
+            break
      
-chave = letras.upper() + f"{random.randint(1000, 9999)}"
+    chave = letras.upper() + f"{random.randint(1000, 9999)}"
 
-print("Sua chave é:", chave)
+    print("Sua chave é:", chave)
 
-x = 0
+    x = 0
 
     while x != 2 and x != 11:
         x = int(input("\nDigite 2 para votação ou 11 para gerenciamento: "))
@@ -33,4 +36,13 @@ x = 0
 
         else:
             print("Opção inválida\n")
+
     return chave
+
+def menu_votacao():
+    print("Menu votação")
+
+def menu_gerenciamento():
+    print("Menu gerenciamento")
+
+gerar_chave(menu_votacao, menu_gerenciamento)
