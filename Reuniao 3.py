5697b2079e42fa9ab801d4580c67a1ecd6de6186
gustavomm18 <gugu.missawa@gmail.com>
2026-05-09 18:41:55 -0300

    Update requirements for voting system
    Added requirements for voting verification and logging.

--- Reuniao 3.py
+++ Reuniao 3.py
@@ -1,3 +1,19 @@
 Requisitos para correção da reuniao 2:
-1- corrigir a verificacao de voto se sim ou não
-2- 
+1- registrar eventos(logs):
+
+-transformar registros em txt.
+[14:32] Usuario 102 votou no candidato 3
+[14:35] Usuario 205 tentou votar novamente
+[14:40] Administrador encerrou votação
+
+2-Verificaçao de voto:
+
+-verificar se ja votou
+-evitar duplicidade
+-detectar fraudes
+
+3-Protocolos:
+
+-depois de votado com sucesso,gera o numero do protocolo.
+-conferir se tem  protocolo ou nao,se tiver, o voto é legitimo,
+se nao,pode ser fraude,qualquer um pode dizer que votou
