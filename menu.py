1940f6a9983bab39916a4e848675356d171f3563
gustavomm18 <gugu.missawa@gmail.com>
2026-05-12 16:40:34 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -4,6 +4,8 @@ from validacoes import pedir_cpf
 from validacoes import verificar_nome
 from conexaobd import buscar   #importa a funcao buscar de conexaobd
 from chave import gerar_chave #importa a funcao de geracao de chave de chave.py
+from protocolo import gerar_protocolo #importa a funcao de geracao de geracao de protocolo em protocolo.py
+
 def menu_gerenciamento(): 
     """
     gerenciamento de eleitores e candidatos
