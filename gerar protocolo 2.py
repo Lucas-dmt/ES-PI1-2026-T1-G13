65a623169607a94b847b6caa616946131226ee4d
gustavomm18 <gugu.missawa@gmail.com>
2026-05-12 16:21:37 -0300

    Create gerar protocolo 2.py

---
+++ gerar protocolo 2.py
@@ -0,0 +1,36 @@
+import random
+import time
+
+
+def gerar_protocolo(candidato):
+
+    letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    protocolo = "V"
+
+    # Gera 2 letras aleatórias
+    for i in range(2):
+        protocolo += letras[random.randint(0, 25)]
+
+    # Adiciona Ano 26
+    protocolo += "26"
+
+    # Adiciona número do candidato
+    protocolo += str(candidato)
+
+    # Adiciona números aleatórios de 5 digitos
+    protocolo += str(random.randint(10000, 99999))
+
+    return protocolo
+
+
+candidato = int(input("Digite o numero do candidato: "))
+
+print("Confirmando voto...")
+
+time.sleep(5)
+
+protocolo = gerar_protocolo(candidato)
+
+print("Voto realizado com sucesso!")
+print("Protocolo:", protocolo)
