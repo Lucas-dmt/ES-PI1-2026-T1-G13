d75ced2b1ea644f3bd9319272fc6a2f533ea5501
LUCAS DA MATA PRADO <lucas.mp7@puccampinas.edu.br>
2026-05-21 15:18:14 -0300

    Update criptografia.py

---
+++ criptografia.py
@@ -0,0 +1,37 @@
+conjunto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+a = 4
+b = 3
+c = 1
+d = 2
+
+def converter_texto_numero(texto):
+    numeros = []
+    for letra in texto:
+        letra = letra.upper()
+
+        if letra in conjunto:
+            numeros.append(conjunto.index(letra))
+    return numeros
+def converter_numero_texto(numeros):
+    texto = ""
+    for numero in numeros:
+        texto += conjunto[numero % len(conjunto)]
+
+    return texto
+def criptografar_hill(texto):
+    numeros = converter_texto_numero(texto)
+    if len(numeros) % 2 != 0:
+        numeros.append(0)
+
+    resultado = []
+    for i in range (0, len(numeros), 2):
+        x = numeros[i]
+        y = numeros[i + 1]
+
+        novo1 = (a * x + b * y) % 36
+        novo2 = (c * x + d * y) % 36
+ 
+        resultado.append(novo1)
+        resultado.append(novo2)
+
+    return converter_numero_texto(resultado)
