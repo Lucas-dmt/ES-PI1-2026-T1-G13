e45ac1121cbc998d8316b8da761b10ee6e7c1776
gustavomm18 <gugu.missawa@gmail.com>
2026-05-24 19:31:44 -0300

    Update chave_protocolo.py

--- chave_protocolo.py
+++ chave_protocolo.py
@@ -1,6 +1,7 @@
 import random
 
 def gerar_protocolo(candidato):
+
     letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
     protocolo = "V"
@@ -9,7 +10,17 @@ def gerar_protocolo(candidato):
         protocolo += letras[random.randint(0, 25)]
 
     protocolo += "26"
-    protocolo += str(candidato)
+
+    if candidato == "NULO":
+        protocolo += "00"
+    else:
+        candidato = str(candidato)
+
+        if len(candidato) == 1:
+            protocolo += "0" + candidato
+        else:
+            protocolo += candidato
+
     protocolo += str(random.randint(10000, 99999))
 
     return protocolo
